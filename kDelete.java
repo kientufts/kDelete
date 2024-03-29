a428db701cf2df5d4867ad7b476b0dd6a303f8d9
Kien Le <letrungkien1991@gmail.com>
2024-03-29 01:33:44 -0700

    List all files in a folder, not yet tested

--- kDelete.java
+++ kDelete.java
@@ -1,3 +1,27 @@
-public Class kDelete{
-	
+import java.io.File;
+
+public class kDelete {
+	public static void main(String[] args){
+
+    }
+
+    private void listFiles(String path){
+        File folder = new File(path);
+
+        if(!folder.exists() || !folder.isDirectory()){
+            System.out.println(STR."\{path} does not exist or not a directory");
+            return;
+        }
+
+        File[] files = folder.listFiles();
+
+        if(files == null){
+            System.out.println(STR."There is no file in \{path}");
+            return;
+        }
+
+        for(File file: files){
+            System.out.println(file.getName());
+        }
+    }
 }
